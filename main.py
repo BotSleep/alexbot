7c8c14a02d33c0ad4fe22799ee566105d0068cf2
Mateo Roman <mateoroman@mail.umkc.edu>
2023-10-24 10:08:27 -0500

    sync global command for slash command should work

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 import discord
 from discord.ext import commands
+from discord import app_commands
 import os
 import asyncio
 import dotenv
 from dotenv import load_dotenv
 import random
 import json
-#import sqlite3
 
 load_dotenv()
-
 counts = {}
 script_directory = os.path.abspath(os.path.dirname(__file__))
-
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-
 intents = discord.Intents.default()
 intents.message_content = True
-
-bot = commands.Bot(command_prefix='!', intents=intents)
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+bot = commands.Bot(command_prefix='!', intents=intents, owner_id=108378541988515840)
 
 def load_stats():
     global counts
@@ -28,7 +26,6 @@ def load_stats():
     except (FileNotFoundError, json.JSONDecodeError):
         counts = {}
 
-# Save stats to the JSON file
 def save_stats():
     with open('imagestats.json', 'w') as json_file:
         json.dump(counts, json_file)
@@ -39,9 +36,19 @@ def addImageCount(filename):
     save_stats()
 
 def getMostCommon():
+    ties = 0
+    tienames = []
     most_common_image = max(counts, key=counts.get)
     most_common_count = counts[most_common_image]
-    return os.path.basename(most_common_image), most_common_count
+    for k,v in counts.items():
+        if v is most_common_count:
+            ties += 1
+            tienames.append(os.path.basename(k.split(".")[0]))
+            print(f'DEBUG: {os.path.basename(k.split(".")[0])} rolled: {v}')
+    if ties > 0:
+        return tienames, most_common_count
+    else:
+        return os.path.basename(most_common_image), most_common_count
 
 @bot.command(name='roll', help='Send random image to chat')
 async def images(ctx):
@@ -62,10 +69,22 @@ async def troll(ctx):
     await ctx.send(f"Uh-oh! You accidentally typed toll! Now you must pay the trolls toll!")
     await ctx.send(file=discord.File(random_image))
 
-@bot.command(name='stats')
+@bot.hybrid_command(name='stats', with_app_command=True, help='displays the current most common characters')
 async def stats(ctx):
     character, count = getMostCommon()
-    await ctx.send(f'{character.rstrip(".jpgne")} is the most common roll with {count} rolls.')
+    if type(character) is list:
+        names = ", ".join(character)
+        await ctx.send(f'There is a tie between {names} with {count} rolls.')
+    else:
+        await ctx.send(f'{character.split(".")[0]} is the most common roll with {count} rolls.')
+
+@bot.command(name='sync', description='Owner only')
+async def sync(ctx):
+    if ctx.author.id == 108378541988515840:
+        await bot.tree.sync()
+        print('Command tree synced.')
+    else:
+        await ctx.message('You must be the owner to use this command!')
 
 if __name__ == "__main__":
     load_stats()
